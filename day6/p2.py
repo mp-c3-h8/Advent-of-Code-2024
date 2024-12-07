f5bc30202b494c860ac7929d132ee43df18ee8ff
mp <no@e.mail>
2024-12-07 04:31:45 +0100

    day6

---
+++ day6/p2.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+import re
+
+path = defaultdict(set)
+obstacles = set()
+M = []
+starty, startx = 0, 0
+directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+
+def move(py: int, px: int, d: int, sabotage: bool = False) -> bool:
+    local_path = defaultdict(set)
+
+    while True:
+        if sabotage:
+            local_path[(py, px)].add(d)
+        else:
+            path[(py, px)].add(d)
+
+        # next possible position
+        ny = py + directions[d][0]
+        nx = px + directions[d][1]
+
+        # terminate if out of bounds
+        if ny >= dimy or nx >= dimx or ny < 0 or nx < 0:
+            return False
+
+        # obstacle in front?
+        if M[ny][nx] == "#":
+            d = (d+1) % 4  # turn right
+        else:
+            if sabotage:
+                # must meet prev path with same direction for a loop
+                if (ny, nx) in path and d in path[(ny, nx)]:
+                    return True
+                # dont forget newly introduced paths
+                if (ny, nx) in local_path and d in local_path[(ny, nx)]:
+                    return True
+            else:
+                # try to sabotage position (ny,nx)
+                M[ny][nx] = "#"
+                # cant put blockage where we already walked!!
+                if (ny, nx) not in path:
+                    # distinct
+                    if (ny, nx) not in obstacles:
+                        # cant be starting position
+                        if (ny, nx) != (starty, startx):
+                            # check if we send him into a loop
+                            if move(py, px, (d+1) % 4, True):
+                                # thats a valid obstruction
+                                obstacles.add((ny, nx))
+
+                M[ny][nx] = "."
+
+            py, px = ny, nx  # move on
+
+
+for i, row in enumerate(open("input.txt").read().splitlines()):
+    found = re.search(r"\^", row)
+    if found:
+        starty, startx = i, found.start()
+    M.append(list(row))
+
+dimy, dimx = len(M), len(M[0])
+
+move(starty, startx, 0)
+
+print(len(path))
+print(len(obstacles))

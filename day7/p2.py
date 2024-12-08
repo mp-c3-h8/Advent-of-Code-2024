fd3e5f3cd0de9e1be363d2580c00a076dc9833bd
mp <no@e.mail>
2024-12-08 04:41:42 +0100

    day7 opt

--- day7/p2.py
+++ day7/p2.py
@@ -8,16 +8,21 @@ def mult(x: int, y: int) -> int: return x*y
 def cat(x: int, y: int) -> int: return 10**int(log(y, 10)+1)*x+y
 
 
-def treelike(l: list[int], target: int, ops: list[Callable]) -> int:
+def treelike(l: list[int], target: int, ops: tuple[Callable[[int, int], int], ...]) -> int:
     N = len(l)-2
     arr = [l[0]]
     def f(j): return (op(a, l[j+1]) for op in ops for a in arr)
     for i in range(N):
-        arr = list(f(i))
+        narr = []
+        for n in f(i):
+            # skip already too large numbers
+            if n <= target:
+                narr.append(n)
+        arr = narr
 
     # dont need big array for last step
-    for b in f(N):
-        if b == target:
+    for n in f(N):
+        if n == target:
             return target
     return 0
 
@@ -25,5 +30,5 @@ def treelike(l: list[int], target: int, ops: list[Callable]) -> int:
 data = [list(map(int, re.findall(r"\d+", l)))
         for l in open("input.txt").readlines()]
 
-print(sum(treelike(line[1::], line[0], [add, mult]) for line in data))
-print(sum(treelike(line[1::], line[0], [add, mult, cat]) for line in data))
+print(sum(treelike(line[1::], line[0], (add, mult)) for line in data))
+print(sum(treelike(line[1::], line[0], (add, mult, cat)) for line in data))

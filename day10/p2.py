271c2db225556d749314d494b56d089882c3a2ef
mp <no@e.mail>
2024-12-10 17:58:34 +0100

    day10

---
+++ day10/p2.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+data: list[list[int]] = []
+heads: list[tuple[int, int]] = []
+score = 0
+rating = 0
+
+for j, line in enumerate(open("input.txt").read().splitlines()):
+    data.append(row := list(map(int, line)))
+    for i, n in enumerate(map(int, row)):
+        if n == 0:
+            heads.append((j, i))
+
+nrows = len(data)
+ncols = len(data[0])
+
+
+def is_valid(y: int, x: int) -> bool:
+    return x >= 0 and x < ncols and y >= 0 and y < nrows
+
+
+def ascend(starts: Counter[tuple[int, int]], from_height: int) -> Counter[tuple[int, int]]:
+    res = Counter()
+    for (y, x), rating in starts.items():
+        candidates = ((y+1, x), (y-1, x), (y, x+1), (y, x-1))
+        for (ay, ax) in ((cy, cx) for cy, cx in candidates if is_valid(cy, cx) and data[cy][cx] == from_height+1):
+            res += {(ay, ax): rating}
+    return res
+
+
+for counter in (Counter({h: 1}) for h in heads):
+    for i in range(9):
+        if not counter:
+            break
+        counter = ascend(counter, i)
+    score += len(counter)
+    rating += counter.total()
+    
+print(score)
+print(rating)

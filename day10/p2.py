98500b21447966acba36207f9d9db3a94453343d
mp <no@e.mail>
2024-12-10 20:20:59 +0100

    day10

--- day10/p2.py
+++ day10/p2.py
@@ -1,31 +1,29 @@
 from collections import Counter
 type Count = Counter[tuple[int, int]]
+type Coords = tuple[int, int]
 
-data: list[list[int]] = []
-heads: list[tuple[int, int]] = []
+grid: dict[Coords, int] = dict()
+heads: list[Coords] = []
 score = 0
 rating = 0
 
 for j, line in enumerate(open("input.txt").read().splitlines()):
-    data.append(row := list(map(int, line)))
-    for i, n in enumerate(row):
+    for i, n in enumerate(list(map(int, line))):
+        grid[(j, i)] = n
         if n == 0:
             heads.append((j, i))
 
-nrows = len(data)
-ncols = len(data[0])
 
-
-def is_valid(y: int, x: int) -> bool:
-    return x >= 0 and x < ncols and y >= 0 and y < nrows
+def getCandidates(c: Coords) -> list[Coords]:
+    y, x = c
+    return [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]
 
 
 def ascend(starts: Count, from_height: int) -> Count:
     res: Count = Counter()
-    for (y, x), rating in starts.items():
-        candidates = ((y+1, x), (y-1, x), (y, x+1), (y, x-1))
-        for (ay, ax) in ((cy, cx) for cy, cx in candidates if is_valid(cy, cx) and data[cy][cx] == from_height+1):
-            res += {(ay, ax): rating}
+    for s, rating in starts.items():
+        for c in (c for c in getCandidates(s) if c in grid and grid[c] == from_height+1):
+            res += {c: rating}
     return res
 
 
@@ -36,6 +34,6 @@ for counter in (Counter({h: 1}) for h in heads):
         counter = ascend(counter, i)
     score += len(counter)
     rating += counter.total()
-    
-print("score:",score)
-print("rating:",rating)
+
+print("score:", score)
+print("rating:", rating)

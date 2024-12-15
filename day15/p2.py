2052d631392681a3631399c125387413b86e7e72
mp <no@e.mail>
2024-12-16 00:29:23 +0100

    day15

--- day15/p2.py
+++ day15/p2.py
@@ -1,48 +1,45 @@
 
-type Coords = tuple[int, int]  # (y,x)
-type Grid = list[list[str]]
-type Box = tuple[Coords, Coords]
-grid: Grid = []
-robot: Coords = (0, 0)
-curr_dir: Coords = (0, 0)
+type Grid = dict[complex, str]
+type Box = tuple[complex, complex]
+grid: Grid = dict()
+robot: complex = 0
+dz: complex = 0
 
 warehouse, moves = open("input.txt").read().split("\n\n")
 moves = moves.replace("\n", "")
 warehouse = warehouse.splitlines()
 
-for y, line in enumerate(warehouse):
-    row = ""
-    for x, n in enumerate(line):
-        add = n+n
-        if n == "O":
-            add = "[]"
-        elif n == "@":
-            add = "@."
-            robot = (y, 2*x)
-        row += add
-    grid.append(list(row))
 
-DIRS: dict[str, Coords] = {
-    "^": (-1, 0),
-    ">": (0, 1),
-    "v": (1, 0),
-    "<": (0, -1)
+for y, line in enumerate(warehouse):
+    for x, (c1, c2) in enumerate(zip(line, line)):
+        if c1 == "O":
+            c1, c2 = "[", "]"
+        elif c1 == "@":
+            c2 = "."
+            robot = 2*x + y * 1j
+        grid[2*x+y*1j] = c1
+        grid[2*x+1+y*1j] = c2
+
+
+DIRS: dict[str, complex] = {
+    "^": -1j,
+    ">": 1,
+    "v": 1j,
+    "<": -1
 }
 
 
 def cascade_boxes(boxes: list[set[Box]]) -> list[set[Box]] | None:
-    dy, dx = curr_dir
     boxes_to_check = boxes[-1]
     next_boxes_to_check: set[Box] = set()
-    for box in boxes_to_check:
-        for halfbox in box:
-            sy, sx = halfbox[0]+dy, halfbox[1]+dx
-            if grid[sy][sx] == "#":
-                return None
-            elif dx != -1 and grid[sy][sx] == "[":
-                next_boxes_to_check.add(((sy, sx), (sy, sx+1)))
-            elif dx != 1 and grid[sy][sx] == "]":
-                next_boxes_to_check.add(((sy, sx-1), (sy, sx)))
+    for halfbox in (halfbox for box in boxes_to_check for halfbox in box):
+        check = halfbox+dz
+        if grid[check] == "#":
+            return None
+        elif dz != DIRS["<"] and grid[check] == "[":
+            next_boxes_to_check.add((check, check+1))
+        elif dz != DIRS[">"] and grid[check] == "]":
+            next_boxes_to_check.add((check-1, check))
     if next_boxes_to_check:
         boxes.append(next_boxes_to_check)
         return cascade_boxes(boxes)
@@ -50,52 +47,48 @@ def cascade_boxes(boxes: list[set[Box]]) -> list[set[Box]] | None:
         return boxes
 
 
-def box_in_front(sy: int, sx: int) -> Box | None:
-    if grid[sy][sx] == "[":
-        return ((sy, sx), (sy, sx+1))
-    elif grid[sy][sx] == "]":
-        return ((sy, sx-1), (sy, sx))
+def box_in_front(front: complex) -> Box | None:
+    if grid[front] == "[":
+        return (front, front+1)
+    elif grid[front] == "]":
+        return (front-1, front)
     else:
         return None
 
 
 def move_boxes(boxes: list[set[Box]]) -> None:
-    dy, dx = curr_dir
-    for boxes_set in boxes[::-1]:
-        for box in boxes_set:
-            (ly, lx), (ry, rx) = box
-            grid[ly+dy][lx+dx], grid[ry+dy][rx+dx] = grid[ly][lx], grid[ry][rx]
-            if dy:
-                grid[ly][lx], grid[ry][rx] = ".", "."
+    for box in (box for boxes_set in boxes[::-1] for box in boxes_set):
+        l, r = box
+        grid[l+dz], grid[r+dz] = grid[l], grid[r]
+        if dz.imag:
+            grid[l], grid[r] = ".", "."
 
 
 def move() -> None:
     global robot
-    dy, dx = curr_dir
-    sy, sx = robot[0]+dy, robot[1]+dx
-    if grid[sy][sx] == "#":
+    if grid[robot+dz] == "#":
         return
-    if box := box_in_front(sy, sx):
+    if box := box_in_front(robot+dz):
         if not (boxes_to_move := cascade_boxes([{box}])):
             return
         move_boxes(boxes_to_move)
 
-    grid[robot[0]][robot[1]] = "."
-    grid[sy][sx] = "@"
-    robot = (sy, sx)
+    grid[robot] = "."
+    grid[robot+dz] = "@"
+    robot += dz
 
 
 def print_map(move: str) -> None:
     print("Move " + move + ":")
-    print("".join("".join(row) + "\n" for row in grid), "\n")
+    gen = ((val, z.real == len(warehouse[0])*2-1) for z, val in grid.items())
+    print("".join(val + "\n" * newline for val, newline in gen))
 
 
 for m in moves:
-    curr_dir = DIRS[m]
+    dz = DIRS[m]
     move()
-    #print_map(m)
-    
+    # print_map(m)
+
 print_map("finished")
 
-print(sum((y*100+x) * (val == "[")
-      for y, line in enumerate(grid) for x, val in enumerate(line)))
+print(int(sum((z.imag*100+z.real) for z, val in grid.items() if val == "[")))
